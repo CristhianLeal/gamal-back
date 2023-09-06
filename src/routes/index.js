46825a4277d578213f8678c65862f92f1478714a
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-09-06 20:28:53 -0300

    update server and index with postscontrollers, model and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
 export { default as usersRoutes } from './users.routes.js'
 export { default as personsRoutes } from './persons.routes.js'
 export { default as productsRoutes } from './products.routes.js'
+export { default as postsRoutes } from './posts.routes.js'
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { usersRoutes, personsRoutes, productsRoutes } from './routes/index.js'
+import { usersRoutes, personsRoutes, productsRoutes, postsRoutes } from './routes/index.js'
 import cors from 'cors'
 import { dbConnection } from './db/config.js'
 
@@ -24,6 +24,7 @@ export class Server {
     this.app.use('/users', usersRoutes)
     this.app.use('/persons', personsRoutes)
     this.app.use('/products', productsRoutes)
+    this.app.use('/posts', postsRoutes)
   }
 
   listen () {
