d2af59131fd411742a0b0e84d461fad05cfa9ab8
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-10-19 21:52:16 -0300

    userRoutes update with editUser

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { getUsers, getUser, createUser, deleteUser, loginUser } from '.././controllers/users.controllers.js'
+import { getUsers, getUser, createUser, deleteUser, loginUser, editUser } from '.././controllers/users.controllers.js'
 import { validateToken } from '../middlewares/validatesToken.js'
 const router = Router()
 
@@ -8,5 +8,6 @@ router.get('/:id', validateToken, getUser)
 router.post('/', createUser)
 router.delete('/:id', validateToken, deleteUser)
 router.post('/login', loginUser)
+router.put('/:id', validateToken, editUser)
 
 export default router
